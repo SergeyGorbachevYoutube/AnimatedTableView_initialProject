b1135e3118463f5fea32c04f83029a511e94ebc0
yaSkazalGorbatiy <ya.skazal.gorbatiy@gmail.com>
2021-09-22 11:58:34 +0300

    Initial Commit

---
+++ AnimatedTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedTableViewCell
+//
+//  Created by Сергей Горбачёв on 22.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
